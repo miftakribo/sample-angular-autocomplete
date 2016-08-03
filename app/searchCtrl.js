5a1649b2f5ae2ebcbd6b56c2220ed1f122603e74
miftah <dmiftaranger@gmail.com>
2016-08-03 09:23:08 +0700

    creating directive

---
+++ app/searchCtrl.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name v2App.controller:BrandCtrl
+ * @description
+ * # BrandCtrl
+ * Controller of the v2App
+ */
+angular.module('v2App')
+  .controller('SearchCtrl', searchCtrl);
+  function searchCtrl($scope) {
+    $result1= 
+  }
